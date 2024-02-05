cf123353793578a4d07e53658fb0329534585363
Mikalai Rusakovich <kolyos83@mail.ru>
2024-02-06 02:02:14 +0300

    feat: add error handling to single-word commands

--- src/app/commands/ls.js
+++ src/app/commands/ls.js
@@ -6,15 +6,19 @@ import * as path from "path";
 const readdir = promisify(fs.readdir);
 const stat = promisify(fs.stat);
 
-export async function ls() {
-    const files = await readdir(__dirname());
-    const result = await Promise.all(files.map(async file => {
-        const statFile = await stat(path.join(__dirname(), file));
-        return {
-            name: file,
-            type: statFile.isFile() ? 'file' : 'directory'
-        }
-    }));
-    console.table(result.sort((file1, file2) => file1.type.localeCompare(file2.type)));
-    return 'Done!\n';
+export async function ls(args) {
+    if (!args) {
+        const files = await readdir(__dirname());
+        const result = await Promise.all(files.map(async file => {
+            const statFile = await stat(path.join(__dirname(), file));
+            return {
+                name: file,
+                type: statFile.isFile() ? 'file' : 'directory'
+            }
+        }));
+        console.table(result.sort((file1, file2) => file1.type.localeCompare(file2.type)));
+        return 'Done!\n';
+    } else {
+        throw new Error('Invalid input');
+    }
 }
--- src/app/commands/up.js
+++ src/app/commands/up.js
@@ -1,11 +1,15 @@
 import { __dirname } from "../utils/naming.js";
 
-export function up() {
-    const upFolderPath = __dirname()
-        .split('\\')
-        .slice(0, -1)
-        .join('\\');
-    process.chdir(`${upFolderPath}\\`);
+export function up(args) {
+    if (!args) {
+        const upFolderPath = __dirname()
+            .split('\\')
+            .slice(0, -1)
+            .join('\\');
+        process.chdir(`${upFolderPath}\\`);
+    } else {
+        throw new Error('Invalid input');
+    }
 
     return '';
 }
